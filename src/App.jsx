4d981c09d1e2cec21d92205e5eee8ba98ec9a14c
Shlomi <shlomiasssh1999@gmail.com>
2024-01-22 13:15:09 +0200

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Signin from "./routes/signin";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import SignUp from "./routes/signUp";
+import About from "./routes/about";
 
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/routes/about.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function about() {
+  return <h1 className="text-3xl font-bold underline">about</h1>;
+}
